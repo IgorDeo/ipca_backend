fe226bf674d6c4b8281452ac94328bd5bfbd28cb
Igor Deo Alves <igor.deo@grana.capital>
2022-04-07 18:23:15 -0300

    server port change

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from server import Server
+import os
 
 if __name__ == '__main__':
     server = Server(__name__)
-    server.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000)) 
+    server.run(host='0.0.0.0', port=port)
 
